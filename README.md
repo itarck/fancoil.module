e3e820461d75b3734034378db902034d999f0d25
itarck <itarck@gmail.com>
2021-12-17 16:46:45 +0800

    Release 0.0.2-SNAPSHOT

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # fancoil.module
 
 Modules for fancoil framework.
+
+
+## Installation
+
+Leiningen/Boot
+
+    [com.github.itarck/fancoil.module "0.0.2-SNAPSHOT"]
+
+Clojure CLI/deps.edn
+
+    com.github.itarck/fancoil.module {:mvn/version "0.0.2-SNAPSHOT"}
+
--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
-(defproject com.github.itarck/fancoil.module "0.0.1-SNAPSHOT"
+(defproject com.github.itarck/fancoil.module "0.0.2-SNAPSHOT"
   :description "Modules for fancoil framework "
   :url "https://github.com/itarck/fancoil.module"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
-  :source-paths ["src/main"])
+  :source-paths ["src/main"]
+  :dependencies [[applied-science/js-interop "0.3.1"]
+                 [cljs-http/cljs-http  "0.1.46"]
+                 [datascript/datascript "1.3.2"]
+                 [denistakeda/posh "0.5.9"]
+                 [integrant/integrant "0.8.0"]
+                 [reagent/reagent "1.1.0"]
+                 [com.github.itarck/fancoil "0.0.2-SNAPSHOT"]])
