a034f00cb5b7da15597a31c78e22f14b4ba8b5f9
itarck <itarck@gmail.com>
2021-12-24 18:45:23 +0800

    Add cljs-ajax PUT, DELETE

--- src/main/fancoil/module/cljs_ajax/plugin.cljs
+++ src/main/fancoil/module/cljs_ajax/plugin.cljs
@@ -2,44 +2,27 @@
   (:require
    [cljs.core.async :refer [go]]
    [fancoil.base :as base]
-   [ajax.core :refer [json-request-format json-response-format GET POST]]
+   [ajax.core :refer [json-request-format json-response-format GET POST PUT DELETE]]
    [ajax.simple :refer [ajax-request]]))
 
 
-(defmethod base/do! :ajax/get
-  [config _ effect]
-  (go
-    (let [{:keys [uri opt callback] :or {opt {}}} effect
-          callback-handler (if (fn? callback)
-                             callback
-                             (fn [response]
-                               (let [req #:request {:method callback
-                                                    :event response}]
-                                 (base/do! config :dispatch/request req))))
-          opt (assoc opt :handler callback-handler)]
-      (GET uri opt))))
+;; helers
 
-(defmethod base/do! :ajax/post
-  [config _ effect]
-  (go
-    (let [{:keys [uri opt callback] :or {opt {}}} effect
-          callback-handler (if (fn? callback)
-                             callback
-                             (fn [response]
-                               (let [req #:request {:method callback
-                                                    :event response}]
-                                 (base/do! config :dispatch/request req))))
-          opt (assoc opt :handler callback-handler)]
-      (POST uri opt))))
-
-
-(def default-request
+(def default-request 
   {:format          (json-request-format)
    :response-format (json-response-format {:keywords? true})})
 
 
+(defn make-callback-fn [core callback-keyword]
+  (fn [response]
+    (let [req #:request {:method callback-keyword
+                         :event response}]
+      (base/do! core :dispatch/request req))))
+
+;; base functions
+
 (defmethod base/do! :ajax/request
-  [config _ effect]
+  [core _ effect]
   (go
     (let [{:keys [request callback]} effect
           callback-fn (if (fn? callback)
@@ -48,11 +31,55 @@
                           (if ok
                             (let [req #:request {:method callback
                                                  :event response}]
-                              (base/do! config :dispatch/request req))
+                              (base/do! core :dispatch/request req))
                             (js/console.error (str response)))))
           merged-request (->
                           (merge default-request request)
                           (assoc :handler callback-fn))]
       (ajax-request merged-request))))
+
+
+(defmethod base/do! :ajax/get
+  [core _ effect]
+  (go
+    (let [{:keys [uri opt callback] :or {opt {}}} effect
+          callback-handler (if (fn? callback)
+                             callback
+                             (make-callback-fn core callback))
+          opt (assoc opt :handler callback-handler)]
+      (GET uri opt))))
+
+(defmethod base/do! :ajax/post
+  [core _ effect]
+  (go
+    (let [{:keys [uri opt callback] :or {opt {}}} effect
+          callback-handler (if (fn? callback)
+                             callback
+                             (make-callback-fn core callback))
+          opt (assoc opt :handler callback-handler)]
+      (POST uri opt))))
+
+(defmethod base/do! :ajax/put
+  [core _ effect]
+  (go
+    (let [{:keys [uri opt callback] :or {opt {}}} effect
+          callback-handler (if (fn? callback)
+                             callback
+                             (make-callback-fn core callback))
+          opt (assoc opt :handler callback-handler)]
+      (PUT uri opt))))
+
+
+(defmethod base/do! :ajax/delete
+  [core _ effect]
+  (go
+    (let [{:keys [uri opt callback] :or {opt {}}} effect
+          callback-handler (if (fn? callback)
+                             callback
+                             (make-callback-fn core callback))
+          opt (assoc opt :handler callback-handler)]
+      (DELETE uri opt))))
+
+
 
 
--- src/test/fancoil/module/cljs_ajax/test_plugin.cljs
+++ src/test/fancoil/module/cljs_ajax/test_plugin.cljs
@@ -14,27 +14,6 @@
   (str/join "/" (cons api-url params)))
 
 
-;; GET
-
-(comment
-  (base/do! {} :ajax/get {:uri (endpoint "articles")
-                          :opt {:keywords? true}
-                          :callback prn}))
-
-;; POST
-
-(comment
-
-  (base/do! {} :ajax/post {:uri (endpoint "users")
-                           :opt {:params {:user {:username "Jake16"
-                                                 :email "jake16@gmail.com"
-                                                 :password "jakejake"}}
-                                 :format :json}
-                           :callback prn})
-  ;; => #object[cljs.core.async.impl.channels.ManyToManyChannel]
-;;   {"user" {"id" 16, "email" "jake16@gmail.com", "username" "Jake16", "image" nil, "bio" nil, "token" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKYWtlMTYiLCJpc3MiOiJqYWtlMTZAZ21haWwuY29tIiwiZXhwIjoxNjQwOTQ2MDIyLCJpYXQiOjE2NDAzNDEyMjJ9.0vTMqxuJwJMLNmfg57aUF_c307dymzpra555-_165L0"}}
-  )
-
 ;; request
 
 (comment
@@ -50,3 +29,68 @@
   ;; => #object[cljs.core.async.impl.channels.ManyToManyChannel]
 ;; [true {:user {:id 17, :email "Jake17@gmail.com", :username "Jake17", :image nil, :bio nil, :token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKYWtlMTciLCJpc3MiOiJKYWtlMTdAZ21haWwuY29tIiwiZXhwIjoxNjQwOTQ2MDUzLCJpYXQiOjE2NDAzNDEyNTN9.l3E2mzjVNDrywR58F6M2sVAfHnYGG-RUaNiIJHsClZE"}}]
   )
+
+
+;; GET
+
+(comment
+  (base/do! {} :ajax/get {:uri (endpoint "articles")
+                          :opt {:keywords? true}
+                          :callback prn})
+  )
+
+;; POST
+
+(comment
+
+  (base/do! {} :ajax/post {:uri (endpoint "users")
+                           :opt {:params {:user {:username "Jake16"
+                                                 :email "jake16@gmail.com"
+                                                 :password "jakejake"}}
+                                 :format :json}
+                           :callback prn})
+  ;; => #object[cljs.core.async.impl.channels.ManyToManyChannel]
+;;   {"user" {"id" 16, "email" "jake16@gmail.com", "username" "Jake16", "image" nil, "bio" nil, "token" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKYWtlMTYiLCJpc3MiOiJqYWtlMTZAZ21haWwuY29tIiwiZXhwIjoxNjQwOTQ2MDIyLCJpYXQiOjE2NDAzNDEyMjJ9.0vTMqxuJwJMLNmfg57aUF_c307dymzpra555-_165L0"}}
+ 
+ 
+ (base/do! {} :ajax/post {:uri (endpoint "articles")
+                          :opt {:params {:article
+                                         {:title "How to train your dragon"
+                                          :description "Ever wonder how?"
+                                          :body "You have to believe"
+                                          :tagList ["reactjs", "angularjs", "dragons"]}}
+                                :headers {:Authorization "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpdGFyY2s0IiwiaXNzIjoiaXRhcmNrNEBnbWFpbC5jb20iLCJleHAiOjE2NDA5NDI0NzQsImlhdCI6MTY0MDMzNzY3NH0.7rT05GPxwkUQ8ClsGLhhKlZB7LueDxizI46XuhaZEQI"}
+                                :format :json}
+                          :callback prn})
+ 
+;;   {"article" {"updatedAt" "2021-12-24T10:30:30.806Z", "body" "You have to believe", "createdAt" "2021-12-24T10:30:30.806Z", "author" {"username" "itarck4", "bio" nil, "image" nil, "following" false}, "favorited" false, "id" 5, "slug" "how-to-train-your-dragon-1640341830806", "tagList" ["angularjs" "dragons" "reactjs"], "favoritesCount" 0, "title" "How to train your dragon", "description" "Ever wonder how?"}}
+  
+  )
+
+
+;; PUT
+(comment 
+  
+  (base/do! {} :ajax/put {:uri (endpoint "articles" "how-to-train-your-dragon-1640341830806")
+                          :opt {:params {:article
+                                         {:description "XXXXXXX"}}
+                                :headers {:Authorization "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpdGFyY2s0IiwiaXNzIjoiaXRhcmNrNEBnbWFpbC5jb20iLCJleHAiOjE2NDA5NDI0NzQsImlhdCI6MTY0MDMzNzY3NH0.7rT05GPxwkUQ8ClsGLhhKlZB7LueDxizI46XuhaZEQI"}
+                                :format :json}
+                          :callback prn})
+  ;; => #object[cljs.core.async.impl.channels.ManyToManyChannel]
+;; {"article" {"updatedAt" "2021-12-24T10:35:40.768Z", "body" "You have to believe", "createdAt" "2021-12-24T10:30:30.806Z", "author" {"username" "itarck4", "bio" nil, "image" nil, "following" false}, "favorited" false, "id" 5, "slug" "how-to-train-your-dragon-1640341830806", "tagList" ["angularjs" "dragons" "reactjs"], "favoritesCount" 0, "title" "How to train your dragon", "description" "XXXXXXX"}}
+
+  )
+
+
+;; DELETE
+(comment
+
+  (base/do! {} :ajax/delete {:uri (endpoint "articles" "how-to-train-your-dragon-1640341830806")
+                             :opt {:headers {:Authorization "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpdGFyY2s0IiwiaXNzIjoiaXRhcmNrNEBnbWFpbC5jb20iLCJleHAiOjE2NDA5NDI0NzQsImlhdCI6MTY0MDMzNzY3NH0.7rT05GPxwkUQ8ClsGLhhKlZB7LueDxizI46XuhaZEQI"}}
+                             :callback prn})
+  ;; => #object[cljs.core.async.impl.channels.ManyToManyChannel]
+
+
+  )
+
