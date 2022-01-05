401b41a7b9a19ddc94f114a404d5137cb95a4a27
itarck <itarck@gmail.com>
2022-01-05 12:39:44 +0800

    Remove html router core

--- src/main/fancoil/module/reitit/html_router/core.cljs
+++
@@ -1,5 +0,0 @@
-(ns fancoil.module.reitit.html-router.core
-  (:require 
-   [fancoil.module.reitit.html-router.base]
-   [fancoil.module.reitit.html-router.plugin]
-   [fancoil.module.reitit.html-router.unit]))
